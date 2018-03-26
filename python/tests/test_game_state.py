86c1267f8056ea4bcd12e8c09897d969619fc7de
Huascar <huascarmc@hotmail.com>
2018-03-26 21:16:47 +0100

    added tests for all winning combinations PASSED

--- python/tests/test_game_state.py
+++ python/tests/test_game_state.py
@@ -16,8 +16,30 @@ class TestGameState(unittest.TestCase):
     def test_check_win(self):
         self.assertEqual( self.gameState.check_win(self.board), False )
 
-    def test_check_win_combinations_state(self):
+    def test_check_win_combination_one_state(self):
         self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[0], self.board), False)
+
+    def test_check_win_combination_two_state(self):
+        self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[1], self.board), False)
+
+    def test_check_win_combination_three_state(self):
+        self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[2], self.board), False)
+
+    def test_check_win_combination_four_state(self):
+        self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[3], self.board), False)
+
+    def test_check_win_combination_five_state(self):
+        self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[4], self.board), False)
+
+    def test_check_win_combination_six_state(self):
+        self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[5], self.board), False)
+
+    def test_check_win_combination_seven_state(self):
+        self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[6], self.board), False)
+
+    def test_check_win_combination_eight_state(self):
+        self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[7],self.board), False)
+
 
     def test_check_tie(self):
         self.assertEqual( self.gameState.tie(self.board), False)
