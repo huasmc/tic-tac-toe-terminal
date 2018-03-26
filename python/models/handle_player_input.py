e697f0772ac18ec40a026c01948882f2d4740da3
Huascar <huascarmc@hotmail.com>
2018-03-26 19:58:43 +0100

    test get player spot method handleuserinput passed

--- python/models/handle_player_input.py
+++ python/models/handle_player_input.py
@@ -1,5 +1,22 @@
 class HandlePlayerInput:
+
+    def get_input(self,text):
+        return input(text)
+
     def get_player_spot(self):
-        spot = None
-        while spot is None:
-          spot = int(raw_input())
+        spot = self.get_input('Play')
+        return int(spot)
+
+if __name__ == '__main__':
+    h=HandlePlayerInput()
+    h.get_player_spot()
+
+    # def get_player_token(text):
+    #     return input(text)
+
+# def answer():
+#     ans = get_input('enter yes or no')
+#     if ans == 'yes':
+#         return 'you entered yes'
+#     if ans == 'no':
+#         return 'you entered no'
--- python/tests/test_handle_player_input.py
+++ python/tests/test_handle_player_input.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 import sys
 sys.path.append('/Users/huascar/Projects/8th-light-tictactoe/solution/python/')
 from handle_player_input import HandlePlayerInput
@@ -8,5 +9,20 @@ class TestHandlePlayerInput(unittest.TestCase):
     def setUp(self):
         self.handlePlayerInput = HandlePlayerInput()
 
+    @patch('builtins.input', return_value='0')
+    def test_get_user_spot(self, input):
+        self.assertEqual(self.handlePlayerInput.get_player_spot(), 0)
+
 if __name__ == '__main__':
     unittest.main()
+
+
+
+    # #
+    # @patch('yourmodule.get_input', return_value='yes')
+    # def test_answer_yes(self, input):
+    #     self.assertEqual(answer(), 'you entered yes')
+
+    # @patch('yourmodule.get_input', return_value='no')
+    # def test_answer_no(self, input):
+    #     self.assertEqual(answer(), 'you entered no')
