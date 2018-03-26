150a14a5c26a274abc017b145019c1241e4f30bf
Huascar <huascarmc@hotmail.com>
2018-03-26 15:06:41 +0100

    cleaned game file

--- python/game.py
+++ python/game.py
@@ -33,68 +33,6 @@ class Game:
 
     print "Game over"
 
-  def get_human_spot(self):
-    spot = None
-    while spot is None:
-      spot = int(raw_input())
-      if self.board[spot] != "X" and self.board[spot] != "O":
-        self.board[spot] = self.hum
-      else:
-        spot = None
-
-  def eval_board(self):
-    spot = None
-    while spot is None:
-      if self.board[4] == "4":
-        spot = 4
-        self.board[spot] = self.com
-      else:
-        spot = self.get_best_move(self.board, self.com)
-        if self.board[spot] != "X" and self.board[spot] != "O":
-          self.board[spot] = self.com
-        else:
-          spot = None
-
-  def get_best_move(self, board, next_player, depth = 0, best_score = {}):
-    available_spaces = [s for s in board if s != "X" and s != "O"]
-    best_move = None
-
-    for avail in available_spaces:
-      board[int(avail)] = self.com
-      if self.gameState.game_is_over(board):
-        best_move = int(avail)
-        board[int(avail)] = avail
-        return best_move
-      else:
-        board[int(avail)] = self.hum
-        if self.gameState.game_is_over(board):
-          best_move = int(avail)
-          board[int(avail)] = avail
-          return best_move
-        else:
-          board[int(avail)] = avail
-
-    if best_move:
-      return best_move
-    else:
-      return int(available_spaces[0])
-
-  def three_in_a_row(self, *args):
-    return args[0] == args[1] == args[2] == "X" or \
-        args[0] == args[1] == args[2] == "O"
-
-  def game_is_over(self, b):
-    return self.three_in_a_row(b[0], b[1], b[2]) == 1 or \
-        self.three_in_a_row(b[3], b[4], b[5]) == 1 or \
-        self.three_in_a_row(b[6], b[7], b[8]) == 1 or \
-        self.three_in_a_row(b[0], b[3], b[6]) == 1 or \
-        self.three_in_a_row(b[1], b[4], b[7]) == 1 or \
-        self.three_in_a_row(b[2], b[5], b[8]) == 1 or \
-        self.three_in_a_row(b[0], b[4], b[8]) == 1 or \
-        self.three_in_a_row(b[2], b[4], b[6]) == 1
-
-  def tie(self, b):
-    return len([s for s in b if s == "X" or s == "O"]) == 9
 
 if __name__ == '__main__':
   game = Game()
