614d9b77d42f8f3afc714294e35d91bceebc2b5c
Huascar <huascarmc@hotmail.com>
2018-03-26 20:25:28 +0100

    handlePlayerInput manual test success

--- python/game.py
+++ python/game.py
@@ -5,15 +5,17 @@ from human_player import HumanPlayer
 from computer_player import ComputerPlayer
 from board import Board
 from display_board import DisplayBoard
+from handle_player_input import HandlePlayerInput
 
 class Game:
   def __init__(self):
     self.board = Board()
     self.com = 'O'
-    self.displayBoard = DisplayBoard();
+    self.displayBoard = DisplayBoard()
     self.gameState = GameState()
     self.humanPlayer = HumanPlayer()
     self.computerPlayer = ComputerPlayer()
+    self.handlePlayerInput = HandlePlayerInput()
 
   def start_game(self):
     # start by printing the board
@@ -21,7 +23,11 @@ class Game:
     print "Enter [0-8]:"
     # loop through until the game was won or tied
     while not self.gameState.game_is_over(self.board) and not self.gameState.tie(self.board):
-      self.humanPlayer.play(self.board)
+      spot = None
+      while spot == None:
+          spot = self.handlePlayerInput.get_player_spot()
+      self.humanPlayer.play(self.board, spot)
+      spot = None
       if not self.gameState.game_is_over(self.board) and not self.gameState.tie(self.board):
         self.computerPlayer.eval_board(self.board, self.com, self.gameState)
 
