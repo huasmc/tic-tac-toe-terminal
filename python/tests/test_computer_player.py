5327181931e497af3cf51c8597d4a87910cfba71
Huascar <huascarmc@hotmail.com>
2018-03-30 18:34:13 +0100

    play method for computer tested

--- python/tests/test_computer_player.py
+++ python/tests/test_computer_player.py
@@ -14,7 +14,7 @@ class TestComputerPlayer(unittest.TestCase):
         self.assertEquals( self.computerPlayer.token, 'O')
 
     def test_computerPlayer_play_first_middle(self):
-        self.computerPlayer.play(board)
+        self.computerPlayer.play(self.board)
         self.assertEqual( self.board.grid[4], self.computerPlayer.token )
 
 if __name__ == '__main__':
