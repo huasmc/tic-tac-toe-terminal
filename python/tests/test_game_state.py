ca90c9c71baf036f92350fcf26a6605dd84172f6
Huascar <huascarmc@hotmail.com>
2018-04-04 11:52:19 +0100

    added test check tie returns false

--- python/tests/test_game_state.py
+++ python/tests/test_game_state.py
@@ -58,8 +58,14 @@ class TestGameState(unittest.TestCase):
     def test_check_win_combination_eight_state(self):
         self.assertEqual( self.gameState.check_combination_state(self.board.win_combinations[7],self.board), False)
 
+    def test_check_tie_no_tie(self):
+        self.assertEqual( self.gameState.tie(self.board), False )
+
     def test_check_tie(self):
-        self.assertEqual( self.gameState.tie(self.board), False)
+        self.board.grid = ["X", "O", "X",
+                           "O", "X", "O",
+                           "O", "X", "O"]
+        self.assertEqual( self.gameState.tie(self.board), True )
 
 if __name__ == '__main__':
     unittest.main()
