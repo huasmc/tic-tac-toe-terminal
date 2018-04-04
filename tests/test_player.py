08f945c290589a33c7b4c4060f2b7e6eef806e43
Huascar <huascarmc@hotmail.com>
2018-04-04 21:31:24 +0100

    Added tests player class for methods

--- tests/test_player.py
+++ tests/test_player.py
@@ -9,6 +9,17 @@ class TestPlayer(unittest.TestCase):
     def test_has_token(self):
         self.assertEqual( self.player.token, None )
 
+    def test_has_set_token(self):
+        self.player.set_token('X')
+        self.assertEqual( self.player.token, 'X' )
+
+    def test_has_auto_token(self):
+        self.player.auto_token('X')
+        self.assertEqual( self.player.token, 'O' )
+
+    def test_has_play_method(self):
+        self.assertNotNull( self.player.play() )
+
 
 if __name__ == '__main__':
     unittest.main()
