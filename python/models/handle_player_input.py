b55c978f55abd9d384234b5a9f0be8e8aff5219a
Huascar <huascarmc@hotmail.com>
2018-04-04 12:23:22 +0100

    get first player test cases passed

--- python/models/handle_player_input.py
+++ python/models/handle_player_input.py
@@ -2,13 +2,13 @@ import copy
 class HandlePlayerInput:
 
     def get_player_spot(self, available_spots):
-            spot = input('Choose your move: ')
-            if not spot in available_spots:
-                print("Please choose an available spot on the board.")
-                print("Choose one of these spots [%s]:" % ", ".join(available_spots))
-                return self.get_player_spot(available_spots)
-            else:
-                return int(spot)
+        spot = input('Choose your move: ')
+        if not spot in available_spots:
+            print("Please choose an available spot on the board.")
+            print("Choose one of these spots [%s]:" % ", ".join(available_spots))
+            return self.get_player_spot(available_spots)
+        else:
+            return int(spot)
 
     def get_player_token(self):
         token = input('Choose your token! It can be either X or O: ')
@@ -19,6 +19,15 @@ class HandlePlayerInput:
         else:
             print('Please choose either X or O.')
             self.get_player_token()
+
+    def get_first_player(self):
+        first_player = input('Who would like to play first X or O?.')
+        valid_input = ['O', 'X']
+        if not first_player in valid_input:
+            print('Please choose a valid player token as first player.')
+            return self.get_first_player()
+        else:
+            return first_player
 
 #
 # if __name__ == '__main__':
