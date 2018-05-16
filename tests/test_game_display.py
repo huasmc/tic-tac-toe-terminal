1af75abff2183b3bc82ae94bf29ddbcecc66d30a
Huascar <huascarmc@hotmail.com>
2018-05-16 16:22:20 +0100

    GameDisplay show(text) method fails

--- tests/test_game_display.py
+++ tests/test_game_display.py
@@ -9,11 +9,14 @@ class TestGameDisplay(unittest.TestCase):
         self.gameDisplay = GameDisplay();
         self.board = Board();
 
-    def test_gameDisplay_board(self):
+    def test_gameDisplay_show_board(self):
         self.assertEqual( self.gameDisplay.show(self.board), " %s | %s | %s \n===+===+===\n %s | %s | %s \n===+===+===\n %s | %s | %s \n" % \
             (self.board.grid[0], self.board.grid[1], self.board.grid[2],
                  self.board.grid[3], self.board.grid[4], self.board.grid[5],
                  self.board.grid[6], self.board.grid[7], self.board.grid[8]))
+
+    def test_gameDisplay_show_text(self):
+        self.assertEqual( self.gameDisplay.show("text"), "text")
 
 
 if __name__ == '__main__':
