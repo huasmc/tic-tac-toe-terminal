a4df3a643012d42c2650977858dba94cc995ba27
Huascar <huascarmc@hotmail.com>
2018-05-16 15:59:26 +0100

    removed commented code in human_player class

--- models/human_player.py
+++ models/human_player.py
@@ -3,13 +3,8 @@ class HumanPlayer(Player):
 
   def __init__(self):
       super().__init__()
-# Can create class that takes input from terminal
-# pass input as argument for method. SOLID
 
   def play(self, board, spot):
-    # spot = None
-    # while spot is None:
-    #   spot = int(raw_input())
       if board.grid[spot] != "X" and board.grid[spot] != "O":
         board.grid[spot] = self.token
       else:
