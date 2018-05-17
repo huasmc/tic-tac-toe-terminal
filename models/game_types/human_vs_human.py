69cc395b7804edf40a6c55b4e08f681f662dedd7
Huascar <huascarmc@hotmail.com>
2018-05-17 12:25:09 +0100

    removed methods from human_vs_human class

--- models/game_types/human_vs_human.py
+++ models/game_types/human_vs_human.py
@@ -1,19 +1,13 @@
 import copy
 from ..human_player import HumanPlayer
 from ..abstracts.game_type import GameType
+from ..game_display import GameDisplay
 
 class HumanVsHuman(GameType):
   def __init__(self):
     super().__init__()
     self.playerOne= HumanPlayer()
     self.playerTwo = HumanPlayer()
-
-  def play(self):
-    self.set_up()
-    GameDisplay.show(self.board)
-    self.start()
-    GameDisplay.show(self.board)
-    GameDisplay.log('Game Over')
 
   def start(self):
      while not self.gameState.finished(self.board):
@@ -34,20 +28,3 @@ class HumanVsHuman(GameType):
       spot = copy.deepcopy(try_spot)
       player.play(self.board, spot)
       return spot
-
-  def set_up(self):
-      self.set_tokens()
-      self.handleTurns.currentPlayerToken = self.handlePlayerInput.get_first_player()
-
-  def set_tokens(self):
-      while self.playerOne.token == None and self.playerTwo.token == None:
-          player1_token = self.handlePlayerInput.get_player_token()
-          self.playerOne.set_token(player1_token)
-          self.playerTwo.auto_token(player1_token)
-
-  def end_game(self):
-     if (self.gameState.check_win(self.board)[0]):
-       winner = self.gameState.check_win(self.board)[1][0]
-       GameDisplay.log(f"Human {winner} won!")
-     elif (self.gameState.finished(self.board)):
-       GameDisplay.log("It's a tie!")
