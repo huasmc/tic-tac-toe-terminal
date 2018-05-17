06b7df55d676330c789b20556b1ea71a2c3dbf7b
Huascar <huascarmc@hotmail.com>
2018-05-17 14:01:43 +0100

    human_vs_bot inherit tests passed

--- models/game_types/human_vs_bot.py
+++ models/game_types/human_vs_bot.py
@@ -9,23 +9,3 @@ class HumanVsBot(GameType):
     super().__init__()
     self.playerOne = HumanPlayer()
     self.playerTwo = BotPlayer()
-
-  def start(self):
-     while not self.gameState.finished(self.board):
-         if(self.handleTurns.currentPlayerToken == self.playerOne.token):
-             spot = self.let_humanPlayer_play(self.playerOne)
-             GameDisplay.log(f"Human {self.playerOne.token} has played in spot {spot}")
-             GameDisplay.show(self.board)
-             self.handleTurns.change()
-         else:
-             spot = self.playerTwo.play(self.board)
-             GameDisplay.log(f"Bot {self.playerTwo.token} has played in spot {spot}")
-             GameDisplay.show(self.board)
-             self.handleTurns.change()
-         self.end_game()
-         
-  def let_humanPlayer_play(self, player):
-      try_spot = self.handlePlayerInput.get_player_spot(self.board.get_available_spots())
-      spot = copy.deepcopy(try_spot)
-      player.play(self.board, spot)
-      return spot
