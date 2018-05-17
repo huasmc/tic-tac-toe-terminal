3f01e506aba40056132232e1e0e6dcfab410bb54
Huascar <huascarmc@hotmail.com>
2018-05-17 11:01:10 +0100

    bot vs bot test passed"

--- models/game_types/bot_vs_bot.py
+++ models/game_types/bot_vs_bot.py
@@ -6,24 +6,24 @@ from ..game_display import GameDisplay
 class BotVsBot(GameType):
   def __init__(self):
     super().__init__()
-    self.botPlayer1 = BotPlayer()
-    self.botPlayer2 = BotPlayer()
+    self.firstPlayer = BotPlayer()
+    self.secondPlayer = BotPlayer()
 
   def start(self):
      while not self.gameState.finished(self.board):
-         if(self.handleTurns.currentPlayerToken == self.botPlayer1.token):
-             spot = self.botPlayer1.play(self.board)
-             GameDisplay.log(f"Bot {self.botPlayer1.token} has played in spot {spot}")
+         if(self.handleTurns.currentPlayerToken == self.firstPlayer.token):
+             spot = self.firstPlayer.play(self.board)
+             GameDisplay.log(f"Bot {self.firstPlayer.token} has played in spot {spot}")
              GameDisplay.show(self.board)
              self.handleTurns.change()
          else:
-             spot = self.botPlayer2.play(self.board)
-             GameDisplay.log(f"Bot {self.botPlayer2.token} has played in spot {spot}")
+             spot = self.secondPlayer.play(self.board)
+             GameDisplay.log(f"Bot {self.secondPlayer.token} has played in spot {spot}")
              GameDisplay.show(self.board)
              self.handleTurns.change()
      self.end_game()
 
   def set_tokens(self):
-      while self.botPlayer2.token == None:
-          self.botPlayer1.set_token(self.handleTurns.currentPlayerToken)
-          self.botPlayer2.auto_token(self.botPlayer1.token)
+      while self.secondPlayer.token == None:
+          self.firstPlayer.set_token(self.handleTurns.currentPlayerToken)
+          self.secondPlayer.auto_token(self.firstPlayer.token)
