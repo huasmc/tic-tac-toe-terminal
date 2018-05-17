4f881a788611169cced097926dbcb8ac9e68ff6c
Huascar <huascarmc@hotmail.com>
2018-05-17 12:17:48 +0100

    added test suite for human vs human game type

--- core-test.py
+++ core-test.py
@@ -10,6 +10,7 @@ from tests.test_human_player import TestHumanPlayer
 from tests.test_player import TestPlayer
 from tests.test_bot_vs_bot import TestBotVsBot
 from tests.test_human_vs_bot import TestHumanVsBot
+from tests.test_human_vs_human import TestHumanVsHuman
 
 if __name__ == '__main__':
     unittest.main()
