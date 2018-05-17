c220bae90837e1ca72ec4dabd5f1af8f01cd2740
Huascar <huascarmc@hotmail.com>
2018-05-17 09:05:05 +0100

    test bot vs bot inherits game state passed"

--- tests/test_bot_vs_bot.py
+++ tests/test_bot_vs_bot.py
@@ -7,7 +7,10 @@ class TestBotVsBot(unittest.TestCase):
         self.game = BotVsBot()
 
     def test_bot_vs_bot_inherits_board(self):
-        self.assertIsNotNone( self.game.board.grid )
+        self.assertIsNotNone( self.game.board )
+
+    def test_bot_vs_bot_inherits_game_state(self):
+        self.assertIsNotNone( self.game.gameState )
 
 if __name__ == '__main__':
     unittest.main()
