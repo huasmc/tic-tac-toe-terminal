d2555a78734ed67e222b0d1bc523adab0668fc5c
huasmc <huasmc@gmail.com>
2022-10-10 19:47:59 -0400

    chore: setup deploy port

--- restful-game.py
+++ restful-game.py
@@ -1,5 +1,4 @@
-import json
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from models.board import Board
 
 from models.bot_player import BotPlayer
@@ -16,3 +15,10 @@ def Game():
     board.set_board(eval(player_board))
     spot = ai_player.play(board)
     return str(spot)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    if (port): 
+        app.run(host="https://tic-tac-toe-flask-ai.herokuapp.com", port=port)
+    else:
+        app.run(host="http://127.0.0.1", port=5000)
